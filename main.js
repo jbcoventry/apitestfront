ccfff195e66c4fdcc22ab8afccbfd2c76b93f962
Jayden <contact@jbcov.com>
2023-05-05 16:29:23 +1000

    fixed

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 import './style.css'
 
-const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${randomNumber()}`);
-if (!response.ok) {
-  throw new Error(response.statusText);
-}
-const data = await response.json();
+async function getData() {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${randomNumber()}`);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const data = await response.json();
+  
+  function randomNumber() {return Math.floor(Math.random() * 100)}
 
-function randomNumber() {return Math.floor(Math.random() * 100)}
-
-
-document.querySelector('#app').innerHTML = `
+  document.querySelector('#app').innerHTML = `
   <div>
   <p> ${data.title} </p>
   </div>
 `
+
+}
+
+getData();
 
