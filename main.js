6441d4632ca10698a2c2523b17dc1fb4c79ab660
Jayden <contact@jbcov.com>
2023-05-09 09:15:36 +1000

    changes

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
-import './style.css'
+import "./style.css";
 
-async function getData() {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${randomNumber()}`);
+async function renderWorkerData() {
+  const response = await fetch(`https://tw.jbc343.workers.dev`);
   if (!response.ok) {
     throw new Error(response.statusText);
   }
   const data = await response.json();
-  
-  function randomNumber() {return Math.floor(Math.random() * 100)}
 
-  document.querySelector('#app').innerHTML = `
+  document.querySelector("#result").innerHTML = `
   <div>
-  <p> ${data.title} </p>
+  <p> ${data.squadName} </p>
   </div>
-`
+`;
+}
+async function postToWorker() {
+  // const data = {
+  //   squadName: "Mega Heroes",
+  //   hometown: "Paris",
+  // };
+  const data = { squadName: "warp", hometown: "bla" };
 
+  const response = await fetch("https://tw.jbc343.workers.dev", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Methods": "*",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+
+  console.log(result.message);
 }
 
-getData();
-
+function init() {
+  renderWorkerData();
+}
+init();
+document.querySelector("#submit-btn").addEventListener("click", postToWorker);
